0d85387089f607d527cc37901021339e93b2c65e
Istiake Ahamed Novel <novelistiakeahamed@gmail.com>
2022-08-22 09:42:19 +0600

    Unit test and write readme.md

---
+++ README.md
@@ -0,0 +1,28 @@
+# Commission fee calculation.
+
+## Installation
+
+Clone this repository
+
+```bash
+git clone https://github.com/istiake5/commision-task.git
+```
+
+Now install composer
+
+```bash
+composer install
+```
+This command to run the script 
+```
+php script.php input.csv
+```
+
+Run automation test
+```
+./bin/phpunit tests
+```
+
+
+
+
--- tests/AppTest.php
+++ tests/AppTest.php
@@ -9,7 +9,20 @@ final class AppTest extends TestCase
 {
     public function testMain(): void
     {
-        
+        $file = 'input.csv';
+        $app = new \App($file);
+        $transactionServices = $app->getTransactionService();
+        $transactionServices->getCurrencyService()->setRates([
+        'EUR' => 1,
+        'JPY' => 129.53,
+        'USD' => 1.1497,
+        ]);
+        $commissions = [];
+        foreach ($transactionServices->calculateCommission()->getCommissions() as $commission)
+        $commissions[] = $commission;
+
+        $this->assertEquals('0.60, 3.00, 0.00, 0.06, 1.50, 0, 0.70, 0.30, 0.30, 3.00, 0.00, 0.00, 8612', implode(', ',
+        $commissions));
     }
 
 }
